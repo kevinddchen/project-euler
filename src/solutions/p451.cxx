e9edc57b924936b9c574c123754b2a6fced760b9
Kevin Chen <79341521+kevinddchen@users.noreply.github.com>
2023-11-09 01:12:59 -0800

    Add p271 (#13)
    * update description 407
    
    * update description 451
    
    * fixup
    
    * app p271
    
    * fixup

--- src/solutions/p451.cxx
+++ src/solutions/p451.cxx
@@ -6,31 +6,64 @@
 
 /*
 
-The strategy is similar to Problem 407: when p != 2, the ring Z_{p^a} is cyclic
-and has two elements which square to 1: 1 and -1. So, the goal is to find all
-"base square roots of 1" corresponding to the choices
-(-1, 1, 1, ...), (1, -1, 1, ...), (1, 1, -1, ...) ...
-and then find the combination with the largest product (that is not -1).
+The strategy is similar to that of Problem 407. Given an integer and its prime
+factorization, n = p1^a1 * p2^a2 * ... * pk^ak, the Chinese Remainder Theorem
+defines a ring isomorphism,
 
-However, this story needs to be modified for Z_8, Z_16, Z_32, ... which are
-actually not cyclic and there are 4 square roots of 1 in each of these rings:
-1, 2^{k-1} - 1, 2^{k-1} + 1, -1
-So we just need to treat this as a special case
+    Z_n = Z_{p1^a1} * Z_{p2^a2} * ... * Z_{pk^ak}
+
+When p != 2, the ring Z_{p^a} is cyclic and has two elements which square to 1:
+1 and -1, but one is just the negative of the other. However, the rings Z_8,
+Z_16, Z_32, ... are not cyclic and there are 4 square roots of 1 in
+each of these rings, two of which are negatives of the others.
+
+    1, 2^{k-1} - 1, 2^{k-1} + 1, -1
+
+So, the goal is to find all "base square roots of 1" corresponding to
+the choices
+
+    (-1, 1, 1, ...), (1, -1, 1, ...), (1, 1, -1, ...) ...
+
+If Z_n contains a special case Z_8, Z_16, Z_32, ... then we also need to add
+an extra "base square root of 1" corresponding to the choice
+
+    (2^{k-1} + 1, 1, 1, ...)
+
+We find the combination of these base roots to find the largest product mod n,
+that is not n-1. See this link for more info:
+https://en.wikipedia.org/wiki/Primitive_root_modulo_n.
+
+(Related problems: 271, 407)
 
 ANSWER 153651073760956
 
 */
 
-/* Recursively find largest root. */
-long find_max_root(long* arr, int size, int N, int i = 0, long running_prod = 1)
+/**
+ * Copied from Problem 407.
+ *
+ * In the ring Z_n, for a given prime factor p^a find the integer 1 <= m < n
+ * such that m = 1 (mod p^a) and m = 0 (mod q^b) for all other prime factors
+ * q^b of n.
+ */
+long get_base_idempotent(long n, const mf::PrimePower& factor)
 {
-    if (i == size) {
-        return running_prod < N - 1 ? running_prod : 1;
+    const long x = mf::pow(factor.base, factor.exp);  // pi^ai
+    const long n_x = n / x;
+    // find b * n_x (mod n) such that b * n_x = 1 (mod x)
+    return (mf::modular_inverse(n_x, x) * n_x) % n;
+}
+
+/* Recursively find largest root. */
+long find_max_root(long* base_roots, int num_roots, int n, int i = 0, long running_prod = 1)
+{
+    if (i == num_roots) {
+        return running_prod < n - 1 ? running_prod : 1;
     }
 
     return std::max(
-        find_max_root(arr, size, N, i + 1, running_prod),
-        find_max_root(arr, size, N, i + 1, (running_prod * arr[i]) % N));
+        find_max_root(base_roots, num_roots, n, i + 1, running_prod),
+        find_max_root(base_roots, num_roots, n, i + 1, (running_prod * base_roots[i]) % n));
 }
 
 long p451()
@@ -50,30 +83,28 @@ long p451()
 
         // find all base roots
         const int num_roots = factors.size() + (non_cyclic ? 1 : 0);
-        auto roots = std::make_unique<long[]>(num_roots);
+        const auto base_roots = std::make_unique<long[]>(num_roots);
+
         int i = 0;
         for (const auto& factor : factors) {
             // first, find the multiplicative unit, like (1, 0, 0, ...)
-            const long q = pow(factor.base, factor.exp);  // pi^ai
-            const long n_q = n / q;
-            // find b * n_q such that b * n_q = 1 (mod q)
-            const long unit = (mf::modular_inverse(n_q, q) * n_q) % n;
+            const long unit = get_base_idempotent(n, factor);
 
             // find the non-cyclic base root, like (1 + 2^{k-1}, 1, 1, ...)
             if (non_cyclic && factor.base == 2) {
                 const long root = 1 + (int)pow(2, factor.exp - 1) * unit;
-                roots[i] = ((root % n) + n) % n;
+                base_roots[i] = ((root % n) + n) % n;
                 i++;
             }
 
             // find the base root, like (-1, 1, 1, ...)
             const long root = 1 - 2 * unit;
-            roots[i] = ((root % n) + n) % n;
+            base_roots[i] = ((root % n) + n) % n;
             i++;
         }
 
         // find largest root
-        sum += find_max_root(roots.get(), num_roots, n);
+        sum += find_max_root(base_roots.get(), num_roots, n);
     }
 
     return sum;
