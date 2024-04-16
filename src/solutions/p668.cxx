ef14d2f11e53c6f09ada0a94ac4de0b214989369
Kevin Chen <79341521+kevinddchen@users.noreply.github.com>
2024-04-16 00:22:28 -0700

    cleanup

--- src/solutions/p668.cxx
+++ src/solutions/p668.cxx
@@ -5,44 +5,36 @@
 
 /*
 
-ANSWER
+Starting with the set {1}, we generate all numbers <= 10^10 by iterating over
+prime numbers in increasing order and multiplying against members of the set.
+For instance, considering 2 gives us all powers of 2,
+
+    {1, 2, 4, 8, 16, ...}
+
+And then considering 3, we generate all numbers that are formed by a product
+of 2 or 3,
+
+    {1, 2, 3, 4, 6, 8, 9, ...}
+
+Note that in this process, if we are multiplying a prime p with a member of the
+set n, the product p*n is square root smooth iff p<n. This is because all
+factors of n are <=p, so the largest prime factor of p*n is p, but p^2 < p*n.
+
+The time complexity of the algorithm described above is pretty bad, so we have
+to do some pruning to get below the 1 minute mark. One important optimization
+is that since we are iterating over primes in increasing order, we can drop
+elements of the set if their product with the prime exceeds the 10^10 limit.
+
+ANSWER 2811077773
 
 */
 
-bool is_smooth(long n)
-{
-    const std::vector<mf::PrimePower> facts = mf::prime_factorize(n);
-    for (const auto& fact : facts) {
-        if (fact.base * fact.base >= n) {
-            return false;
-        }
-    }
-    return true;
-}
-
-long p0()
+long p668()
 {
     constexpr long limit = 10'000'000'000;
 
-    // {
-    //     long count = 1;
-    //     for (long n = 2; n <= limit; ++n) {
-    //         if (is_smooth(n)) {
-    //             // printf("%ld\n", n);
-    //             ++count;
-    //         }
-    //     }
-
-    //     printf("count=%ld\n", count);
-    // }
-
-    printf("===\n");
-    // ------------------------------------------------------------------------
-
-    const long sqrt_limit = sqrt(limit);
-
     // generate primes up to sqrt(limit)
-    const std::vector<bool> sieve = mf::prime_sieve(sqrt_limit + 1);
+    const std::vector<bool> sieve = mf::prime_sieve(sqrt(limit) + 1);
     std::vector<long> primes;
     for (int i = 2; i < sieve.size(); ++i) {
         if (sieve[i]) {
@@ -50,7 +42,7 @@ long p0()
         }
     }
 
-    // keeps track of all numbers encountered
+    // keeps track of elements in the set
     std::vector<long> nums = {1};
 
     // keeps track of count of smooth numbers
@@ -63,7 +55,6 @@ long p0()
             while (num * p <= limit) {
                 new_nums.push_back(num);
                 if (p < num) {
-                    // printf("%ld\n", num * p);
                     ++count;
                 }
                 num *= p;
@@ -72,11 +63,11 @@ long p0()
 
         nums = new_nums;
     }
-    printf("count=%ld\n", count);
-    return 0;
+
+    return count;
 }
 
 int main()
 {
-    printf("%ld\n", p0());
+    printf("%ld\n", p668());
 }
