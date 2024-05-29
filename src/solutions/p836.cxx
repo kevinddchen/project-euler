4e3230f6502441e136790b6a830fd96f9081c7b4
Kevin Chen <79341521+kevinddchen@users.noreply.github.com>
2024-05-28 22:49:07 -0700

    Add p836

---
+++ src/solutions/p836.cxx
@@ -0,0 +1,22 @@
+#include "common.h"
+
+#include <string>
+
+/*
+
+Take the first letter of each bolded word in the problem description:
+https://projecteuler.net/problem=836
+
+ANSWER aprilfoolsjoke
+
+*/
+
+std::string p836()
+{
+    return "aprilfoolsjoke";
+}
+
+int main()
+{
+    printf("%s\n", p836().c_str());
+}
