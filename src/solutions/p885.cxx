6523f6801e19bdff6b3b41efab30a571017a056e
Kevin Chen <79341521+kevinddchen@users.noreply.github.com>
2024-07-03 00:23:35 -0700

    cleanup

--- src/solutions/p885.cxx
+++ src/solutions/p885.cxx
@@ -1,52 +1,24 @@
 #include "common.h"
 
-#include <algorithm>
 #include <array>
 #include <functional>
-#include <vector>
 
 /*
 
-ANSWER
+Our strategy is to iterate over all choices of 18 digits, and then for each
+choice, count the numbers formed by taking all permutations of digits. This
+works because each choice of digits has a single unique representation for f().
+
+ANSWER 827850196
 
 */
 
-// long f(long x)
-// {
-//     // get non-zero digits of x
-//     std::vector<long> digits;
-//     while (x > 0) {
-//         const auto dv = std::div(x, 10L);
-//         if (dv.rem != 0) {
-//             digits.push_back(dv.rem);
-//         }
-//         x = dv.quot;
-//     }
-//     // sort the digits
-//     std::sort(digits.begin(), digits.end());
-//     // from sorted digits, form the number
-//     long result = 0;
-//     for (auto digit : digits) {
-//         result = 10 * result + digit;
-//     }
-//     return result;
-// }
-
-// long S(int num_digits)
-// {
-//     const long limit = std::pow(10, num_digits);
-//     long sum = 0;
-//     for (long x = 1; x < limit; ++x) {
-//         sum += f(x);
-//     }
-//     return sum;
-// }
-
 /**
- * Compute factorial.
+ * Compute factorial. Slow, but good enough.
  */
 long factorial(long n)
 {
+    assert(n < 20);  // likely can't compute factorials larger than this
     long prod = 1;
     for (long i = 2; i <= n; ++i) {
         prod *= i;
@@ -55,9 +27,9 @@ long factorial(long n)
 }
 
 /**
- * Given a set of digits and their counts, compute the total number of
- * permutations, including leading zeros. This equals the multinomial
- * coefficient.
+ * Given a set of digits and their counts, count the numbers formed by taking
+ * all permutations of digits, including leading zeros. This equals the
+ * multinomial coefficient.
  */
 long num_permutations(const std::array<int, 10>& digit_counts, int num_digits)
 {
@@ -83,8 +55,11 @@ long digit_counts_to_number(const std::array<int, 10>& digit_counts)
     return result;
 }
 
-long S(int num_digits, long mod)
+long p885()
 {
+    const int num_digits = 18;
+    const long mod = 1123455689;
+
     long sum = 0;
 
     /**
@@ -100,6 +75,7 @@ long S(int num_digits, long mod)
         if (curr_digit == 9) {
             // make all remaining digits 9s
             digit_counts[curr_digit] = num_digits - curr_count;
+
             const long number = digit_counts_to_number(digit_counts) % mod;
             const long num_perms = num_permutations(digit_counts, num_digits) % mod;
             sum = (sum + number * num_perms) % mod;
@@ -114,15 +90,6 @@ long S(int num_digits, long mod)
     recurse(0, 0);
 
     return sum;
-}
-
-
-long p885()
-{
-    constexpr int num_digits = 18;
-    constexpr long mod = 1123455689;
-
-    return S(num_digits, mod);
 }
 
 int main()
